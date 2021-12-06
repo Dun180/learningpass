c1979f5b2cf474ba1ee0ccb761fe60b892e8ecd0
Dun180 <74287755+Dun180@users.noreply.github.com>
2021-12-06 14:39:32 +0800

    push current files

---
+++ learningpass-spring/src/main/java/com/dun/LearningpassSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningpassSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningpassSpringApplication.class, args);
+    }
+
+}
